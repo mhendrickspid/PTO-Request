df1798df9751797dfb2aecbc38f79dfe3252ce1f
hendricksmd <mhendricks@outlook.com>
2015-07-14 12:04:37 -0600

    Added Fields
    Added fields for time item, subsidiary, and department.
    grabs the user's department and subsidiary.

--- PI_SLET_PTOReq.js
+++ PI_SLET_PTOReq.js
@@ -16,18 +16,26 @@ function ptoRequest(request, response) {
 		var frmPTO = nlapiCreateForm('Request Time Off', false); // create form
 		var userId = nlapiGetUser(); // get user id
 		var supId = nlapiLookupField('employee', userId, 'supervisor'); // get the user supervisor id
+		var subId = nlapiLookupField('employee', userId, 'subsidiary');
+		var dptId = nlapiLookupField('employee', userId, 'department');
 		
 		// add form fields
 		var fldEmployee = frmPTO.addField('custpage_emp', 'select', 'Employee', 'employee');
 		var fldSupervisor = frmPTO.addField('custpage_supervisor', 'select', 'Supervisor', 'employee');
 		var fldType = frmPTO.addField('custpage_type', 'select', 'Payroll Type', 'payrollitem').setMandatory(true);
+		var fldSubsidiary = frmPTO.addField('custpage_subsidiary', 'select', 'Subsidiary', 'subsidiary');
+		var fldDepartment = frmPTO.addField('custpage_department', 'select', 'Department', 'department');
 		// add select options to payroll item type
 		
-		// set field defaults
+		// set field defaults & display types
 		fldEmployee.setDefaultValue(userId);
 		fldEmployee.setDisplayType('inline');
 		fldSupervisor.setDefaultValue(supId);
 		fldSupervisor.setDisplayType('inline');
+		fldSubsidiary.setDefaultValue(subId);
+		fldSubsidiary.setDisplayType('inline');
+		fldDepartment.setDefaultValue(dptId);
+		fldDepartment.setDisplayType('inline');
 		
 		// add sublist & columns
 		var slPTO = frmPTO.addSubList('custpage_timelist', 'inlineeditor', 'Time Off');
@@ -35,14 +43,23 @@ function ptoRequest(request, response) {
 		//var colEndDate = slPTO.addField('custlist_to', 'date', 'To:').setMandatory(true);
 		var colHrs = slPTO.addField('custlist_hours', 'float', 'Hours').setMandatory(true);
 		var colDescription = slPTO.addField('custlist_des', 'textarea', 'Description').setMandatory(true);
+		var colTimeItem = slPTO.addField('custlist_timeitem', 'select', 'Time Item', 'customlist_timeitems').setMandatory(true);
 		
 		frmPTO.addSubmitButton('Submit Request');
 		
 		response.writePage(frmPTO);		
 		
 	} else if (request.getMethod() == 'POST') {//test
+		// Get Main Fields
+		var stEmployeeId = request.getParameter('custpage_emp');
+		var stSupervisor = request.getParameter('custpage_supervisor');
+		var stPayrollId = request.getParameter('custpage_type');
+		var stSubsidiary = request.getParameter('custpage_subsidiary');
+		var stDepartment = request.getParameter('custpage_department');
+		
 		// create custom records		
 		var timeListCount = request.getLineItemCount('custpage_timelist');
+		
 		
 		for (var i = 1; i <= timeListCount; i++) {
 			
@@ -50,9 +67,7 @@ function ptoRequest(request, response) {
 			var dteStart = request.getLineItemValue('custpage_timelist', 'custlist_from', i);
 			//var dteEnd = request.getLineItemValue('custpage_timelist', 'custlist_to', i);
 			var stHours = request.getLineItemValue('custpage_timelist', 'custlist_hours', i);
-			var stEmployeeId = request.getParameter('custpage_emp');
-			var stSupervisor = request.getParameter('custpage_supervisor');
-			var stPayrollId = request.getParameter('custpage_type');
+			var stTimeItem = request.getLineItemValue('custpage_timelist', 'customlist_pi_time_items', i);
 			
 			// Create Custom record
 			var recTimeReq = nlapiCreateRecord('customrecord_pi_pto_request');
@@ -62,9 +77,12 @@ function ptoRequest(request, response) {
 			recTimeReq.setFieldValue('custrecord_supervisor', stSupervisor);
 			recTimeReq.setFieldValue('custrecord_status', 1);
 			recTimeReq.setFieldValue('custrecord_payroll_item', stPayrollId);
-			recTimeReq.setFieldValue('custrecord_from', dteStart);
+			recTimeReq.setFieldValue('custrecord_date', dteStart);
 			//recTimeReq.setFieldValue('custrecord_to', dteEnd);
 			recTimeReq.setFieldValue('custrecord_hours', stHours);
+			recTimeReq.setFieldValue('custrecord_time_item', stTimeItem);
+			recTimeReq.setFieldValue('custrecord_subsidiary', stSubsidiary);
+			recTimeReq.setFieldValue('custrecord_department', stDepartment);
 			
 			// Submit PTO Request Record
 			nlapiSubmitRecord(recTimeReq);
---
+++ PI_WA_AccruePTO.js
@@ -0,0 +1,36 @@
+/**
+ * Module Description
+ * Custom workflow action in the PTO Request workflow.
+ * When a request is approved, decrease the PTO accrued hours by
+ * the amount requested.
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       14 Jul 2015     mhendricks
+ *
+ */
+
+/**
+ * @returns {Void} Any or no return value
+ */
+function ptoAction() {
+	
+	// Get employee's current PTO for the year
+	var employeeId = nlapiGetFieldValue('custrecord_employee');
+	var reqHrs = parseFloat(nlapiGetFieldValue('custrecord_hours'));
+	var currentPTO = parseFloat(nlapiLookupField('employee', employeeId, 'custentity_pi_pto_hrs'));
+		
+	/*
+	 * Decrease the PTO amount whenever a PTO request is approved by a supervisor
+	 * in the PTO Request workflow
+		*/ 
+	try {
+		var newHrs = currentPTO - reqHrs;
+		if (newHrs >= 0){
+			nlapiSubmitField('employee', employeeId, 'custentity_pi_pto_hrs', newHrs);
+		}
+	} catch (e) {
+		nlapiLogExecution('ERROR', 'Unexpected PTO Error', e.toString());
+		throw (e);
+	}
+	
+}
